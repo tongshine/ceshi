f97f81849b417e4fea5d4e817fc9d0617c5a488b
tongshine <111213641+tongshine@users.noreply.github.com>
2022-08-31 22:15:17 +0800

    Add files via upload

---
+++ 图片爬虫.py
@@ -0,0 +1,33 @@
+# 1.定位到迅雷电影资源
+# 2.提取子页面链接地址
+# 3.请求地址拿下载链接
+import time
+
+import requests
+from bs4 import BeautifulSoup
+url = 'https://www.umei.cc/bizhitupian/fengjingbizhi/'
+resp = requests.get(url)
+resp.encoding = 'utf-8'
+# print(resp.text)
+main_page = BeautifulSoup(resp.text, "html.parser")
+alist=main_page.find("div", class_="swiper-wrapper after").find_all("a")
+# print(alist)
+for a in alist:
+    herf = a.get('href')
+
+    herf = 'https://www.umei.cc'+ herf
+    # print(herf)
+    child_resp = requests.get(herf)
+    child_resp.encoding= 'utf-8'
+    child_page = BeautifulSoup(child_resp.text, "html.parser")
+    img= child_alist=child_page.find("div", class_="content-box").find("img")
+    # print(img.get("src"))
+    down_url =img.get("src")
+    down_resp = requests.get(down_url)
+    down_resp.content
+    imge_name = down_url.split("/")[-1]
+    with open("imge/"+imge_name,mode = "wb") as f:
+        f.write(down_resp.content)
+    print("over!", imge_name)
+    time.sleep(1)
+print("all over!!!")
